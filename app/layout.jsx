6c8bc3abfaa8aae42e66d395c7dbebe0097961b4
Jorge <solau1@hotmmail.com>
2026-06-18 00:34:47 -0300

    style: global css and layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,20 @@
+import { Lato, Playfair_Display } from 'next/font/google';
 import './globals.css';
+
+const lato = Lato({
+  subsets: ['latin'],
+  weight: ['300', '400', '700'],
+  variable: '--font-sans',
+  display: 'swap',
+});
+
+const playfair = Playfair_Display({
+  subsets: ['latin'],
+  weight: ['400', '600', '700'],
+  style: ['normal', 'italic'],
+  variable: '--font-serif',
+  display: 'swap',
+});
 
 export const metadata = {
   title: 'Instituto Vanessa Ramos | Liderança & Soberania Feminina',
@@ -7,7 +23,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className={`${lato.variable} ${playfair.variable}`}>
       <body>{children}</body>
     </html>
   );
